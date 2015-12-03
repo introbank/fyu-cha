d0bed5e25731818716bde3997c8ccba4453a6124
Shogo Ishida <sh.zero5@gmail.com>
2015-12-03 22:35:49 +0900

    add gzip js file

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@ var gulp       = require('gulp');
 var browserify = require('gulp-browserify');
 var react      = require('react');
 var uglify     = require('gulp-uglify');
+var gzip     = require('gulp-gzip');
 
 gulp.task('scripts', function() {
   gulp.src('app/*.js')
@@ -10,6 +11,7 @@ gulp.task('scripts', function() {
       transform: ["babelify"],
     }))
     .pipe(uglify())
+    .pipe(gzip())
     .pipe(gulp.dest('./public/javascripts/'));
 });
 
