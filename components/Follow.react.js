be7b8cfe40d8974e7d21365f261824c6a9163e7c
shoe116 <shupoppo.116@gmail.com>
2015-12-20 23:26:09 +0900

    removeの追加

--- components/Follow.react.js
+++ components/Follow.react.js
@@ -6,33 +6,54 @@ var Follow = React.createClass({
   mixins: [ParseReact.Mixin],
 
   observe(props, state) {
+  var followingQuery = new Parse.Query("Following");
+  var col = this.props.account.className.toLowerCase();
+  followingQuery.equalTo("user", Parse.User.current());
+  followingQuery.equalTo(col, {"__type":"Pointer", "className": this.props.account.className, "objectId": this.props.account.objectId});
   return {
       user: ParseReact.currentUser,
+      following: followingQuery,
     };
   },
 
-  follow() {
+  createFollowingObj() {
     var Following = Parse.Object.extend('Following');
     var following = new Following();
     following.set("user", {"__type":"Pointer", "className": this.data.user.className, "objectId": this.data.user.objectId});
     var col = this.props.account.className.toLowerCase();
     following.set(col, {"__type":"Pointer", "className": this.props.account.className, "objectId": this.props.account.objectId});
+    return following; 
+  },
+
+  follow() {
+    var following = this.createFollowingObj();
     return following.save(null, {
       success: function(res){console.log(res.text);},
       error: function(error){console.log(error.text);}});
   },
 
-  unfollow(following){
-    return following.destroy();
+  unfollow() {
+    var Following = Parse.Object.extend('Following');
+    var followings = [];
+    this.data.following.map(function(follow){
+      var following = new Following();
+      following.id = follow.objectId;
+      followings.push(following);
+    });
+    Parse.Object.destroyAll(followings,{
+      success: function(res){console.log(res.text);},
+      error: function(error){console.log(error.description);}});
   },
 
   render() {
+    console.log(this.data.following);
     return (
       <div className="followInfo">
         {this.data.user &&
           <div className="follow_btn">
             <ul>
-              <li>{this.props.account.name}さんを <button className="btn" type="button" onClick={this.follow}>follow</button></li>
+              <li>{this.data.following}{this.props.account.name}さんを <button className="btn" type="button" onClick={this.follow}>follow</button></li>
+              <li>{this.props.account.name}さんを <button className="btn" type="button" onClick={this.unfollow}>unfollow</button></li>
             </ul>
           </div>
         }
