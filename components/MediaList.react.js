52556a7b54b9fea7b66169eec74c7bce4a156195
shoe116 <shupoppo.116@gmail.com>
2015-12-24 22:55:45 +0900

    cssの取り込み

--- components/MediaList.react.js
+++ components/MediaList.react.js
@@ -51,23 +51,38 @@ var MediaList = React.createClass({
   },
 
   render() {
-    return (
-      <div>
-          {this.data.user && 
-            <div class="dashboardPhotosEditStartButton" onClick={this.switchEditMode}>編集</div>}
-        <ul>
+    if(this.state.editMode){ 
+      return (
+        <div>
+          <div className="dashboardPhotosEditStartButton" onClick={this.switchEditMode}>保存</div>
+          <div className="dashboardPhotos editnow">
           {this.data.mediaMap.map(function (mediaMap) {
             return (
-              <li>
-                <img src={mediaMap.media.mediaUri} />
-                {this.state.editMode && <button className="btn" type="button" onClick={this.setIsViewable.bind(this, mediaMap.objectId, true)}>view</button>}
-                {this.state.editMode && <button className="btn" type="button" onClick={this.setIsViewable.bind(this, mediaMap.objectId, false)}>unview</button>}
-              </li>
-            );
-          }, this)}
-        </ul>
-      </div>
-    );
+                  <img src={mediaMap.media.mediaUri} />
+            )}, this) 
+          }
+          </div>
+        </div>
+      )
+    }
+    else {
+      return(
+        <div>
+          {this.data.user &&
+            <div className="dashboardPhotosEditStartButton" onClick={this.switchEditMode}>編集</div>
+          }
+          <ul>
+          {this.data.mediaMap.map(function (mediaMap) {
+          return (
+            <li>
+              <img src={mediaMap.media.mediaUri} />
+            </li>
+          )}, this) 
+          }
+          </ul>
+        </div>
+      )
+    }
   },
 });
 
