e963dcb5798999322cefe50ba6a3408186a65313
Shogo Ishida <sh.zero5@gmail.com>
2015-12-26 11:28:33 +0900

    add highlight navigation bar

--- components/Navigation.react.js
+++ components/Navigation.react.js
@@ -2,13 +2,29 @@ var React      = require('react');
 
 var Navigation = React.createClass({
 
+  getDefaultProps() {
+    return {
+      active: 'none'
+    };
+  },
+
   render() {
+    console.log(this.props.active);
     return (
       <div id="nav" className="cf">
         <ul>
-            <li className="boR" onClick={this.top}>マイページ</li>
-            <li className="boR" onClick={this.artists}>アイドル</li>
+          {this.props.active === 'top' ?
+            <li className="active" onClick={this.top}>マイページ</li> :
+            <li onClick={this.top}>マイページ</li>
+          }
+          {this.props.active === 'idol' ?
+            <li className="active" onClick={this.artists}>アイドル</li> :
+            <li onClick={this.artists}>アイドル</li>
+          }
+          {this.props.active === 'group' ?
+            <li className="active" onClick={this.groups}>グループ</li> :
             <li onClick={this.groups}>グループ</li>
+          }
         </ul>
     </div>
     );
