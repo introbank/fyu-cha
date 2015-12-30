c42d749500e8bc434558b5b43b9dc664eb142184
shoe116 <shupoppo.116@gmail.com>
2015-12-30 11:23:24 +0900

    use display name

--- components/MemberList.react.js
+++ components/MemberList.react.js
@@ -21,15 +21,16 @@ var MemberList = React.createClass({
     if (!this.data.members || this.data.members.length === 0) {
       return <div />;
     }
-
+    var groupName = (this.props.group.displayName) ? this.props.group.displayName : this.props.group.name;
     return (
       <div key={this.props.key}>
         <h2>
           <img src={this.props.group.imageUrl} width="20"/>
-          {this.props.group.name}
+          {groupName}
         </h2>
         <ul>
           {this.data.members.map(function(member) {
+            var artistName = (member.displayName) ?member.displayName : member.name;
             return (
               <li key={member.objectId} >
                 <a href={'/artists/' + member.twitterUsername}><img src={member.imageUrl} /></a>
