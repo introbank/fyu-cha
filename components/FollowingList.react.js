2fedb63f2b409cb12830ecef3841604cf1aa433a
Shogo Ishida <sh.zero5@gmail.com>
2015-12-30 09:07:36 +0900

    fix bug when user follow unexisting account

--- components/FollowingList.react.js
+++ components/FollowingList.react.js
@@ -9,6 +9,9 @@ var Following = React.createClass({
           <h1>アーティスト</h1>
           <ul>
             {this.props.artists.map(function (following) {
+              if (!following.artist) {
+                return <div />;
+              }
               return (
                 <li key={following.objectId} >
                   <a href={'artists/' + following.artist.twitterUsername}><img src={following.artist.imageUrl} /></a>
@@ -23,6 +26,9 @@ var Following = React.createClass({
           <h1>グループ</h1>
           <ul>
             {this.props.groups.map(function (following) {
+              if (!following.group) {
+                return <div />;
+              }
               return (
                 <li key={following.objectId} >
                   <a href={'groups/' + following.group.twitterUsername}><img src={following.group.imageUrl} /></a>
