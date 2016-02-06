f32d9eab6fe0b64bdba9c6af0deca2262976595b
shoe116 <shoe116@Mac-mini.local>
2016-02-06 19:41:42 +0900

    add eventinput with fyucha

---
+++ components/Contribution.react.js
@@ -0,0 +1,68 @@
+var React        = require('react');
+var Parse        = require('../lib/parse');
+var ParseReact   = require('parse-react');
+var AccountInfoLib = require('../lib/AccountInfoLib.js');
+var PageType = require('../lib/PageType.js');
+
+var Contribution = React.createClass({
+  mixins: [ParseReact.Mixin],
+
+  render() {
+    var fyucha = this.props.fyucha;
+    if (PageType.isDashboard(this.props.type) || PageType.isUser(this.props.type) ){
+      var account = null;
+      if (fyucha.artist != null){
+        account = fyucha.artist;
+      }
+      else if (fyucha.group != null){
+        account = fyucha.group;
+      }
+      if (account == null) {
+        return;
+      }
+      return (
+        <div className="fyuchaCommit" key={fyucha.objectId}>
+          <a href={AccountInfoLib.getUrl(account)}>
+            <div className="fyuchaCommitUserArea">
+              <div className="fyuchaCommitUserThumbnail">
+                <img src={AccountInfoLib.getImageUrl(account)} alt={AccountInfoLib.getAccountName(account)} />
+              </div>
+              <p className="fyuchaCommitUserName">{AccountInfoLib.getAccountName(account)}</p>
+              <p className="fyuchaCommitUserAccount">@{AccountInfoLib.getUsername(account)}</p>
+            </div>
+          </a>
+          <div className="fyuchaCommitActionArea">
+            <p className="fyuchaCommitActionText">{fyucha.type}</p>
+            <div className="fyuchaCommitActionNumerals">{fyucha.point}</div>
+          </div>
+        </div>
+      );
+    }
+    else{
+      if (!fyucha.user) {
+        return;
+      }
+      else{
+        return (
+          <div className="fyuchaCommit" key={fyucha.objectId}>
+            <a href={AccountInfoLib.getUrl(fyucha.user)}>
+              <div className="fyuchaCommitUserArea">
+                <div className="fyuchaCommitUserThumbnail">
+                  <img src={fyucha.user.imageUrl} alt={AccountInfoLib.getAccountName(fyucha.user)} />
+                </div>
+                <p className="fyuchaCommitUserName">{AccountInfoLib.getAccountName(fyucha.user)}</p>
+                <p className="fyuchaCommitUserAccount">@{AccountInfoLib.getUsername(fyucha.user)}</p>
+              </div>
+            </a>
+            <div className="fyuchaCommitActionArea">
+              <p className="fyuchaCommitActionText">{fyucha.type}</p>
+              <div className="fyuchaCommitActionNumerals">{fyucha.point}</div>
+            </div>
+          </div>
+        );
+      }
+    }
+  },
+});
+
+module.exports = Contribution;
